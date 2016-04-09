c9b662ff016009b66a543c9809a570cc504dc2a1
David <skodt6@hotmail.com>
2016-04-09 14:11:27 +0200

    Added InControl test scene.

---
+++ unity/Assets/Scripts/CubeController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+
+public class CubeController : MonoBehaviour {
+
+    void Start()
+    {
+        
+        /*foreach (InputDevice o in InputManager.Devices)
+        {
+            Debug.Log("Controller Detected");
+        }
+        */
+    }
+
+        // Update is called once per frame
+        void Update () {
+
+        var InputDevice0 = InputManager.ActiveDevice;
+        transform.Rotate(Vector3.down, 500.0f * Time.deltaTime * InputDevice0.LeftStickX, Space.World);
+        transform.Rotate(Vector3.right, 500.0f * Time.deltaTime * InputDevice0.LeftStickY, Space.World);
+    }
+}
