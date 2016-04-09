3375182f76f8fef4809e0fb2c52156d9f8609a58
Gustav <sir.gustav.the.coder@gmail.com>
2016-04-09 17:20:51 +0200

    Camera script

---
+++ unity/Assets/Scripts/OutOfScreenManager.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using System.Linq;
+
+public class OutOfScreenManager : MonoBehaviour {
+
+	public List<Health> Players = new List<Health>();
+
+	private void PositionAtFurthersPlayer() {
+		var z = Players.Max(health => health.gameObject.transform.position.z);
+		var p = this.transform.position;
+		this.transform.position = new Vector3(p.x, p.y, z);
+	}
+
+	private void KillPlayersWhoAreOutside() {
+		foreach(var p in this.Players) {
+			var z = p.gameObject.transform.position.z;
+			if( z < CameraUtil.CameraPositionBottom ) {
+				p.GetOutsideOfWorld();
+			}
+		}
+	}
+
+	void Update () {
+		this.PositionAtFurthersPlayer();
+		this.KillPlayersWhoAreOutside();
+	}
+}
