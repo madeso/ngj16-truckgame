432cd46ec0691c7d3f7d4f1959d14c9304bca93c
Kasper Lind Sørensen <omglolkkthxbye@gmail.com>
2016-04-09 15:43:34 +0200

    Added Controller class.

---
+++ unity/Assets/Scripts/Controller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using InControl;
+
+public class Controller
+{
+    public InputDevice inputDevice;
+    public int roleId = -1;
+
+    public Controller (InputDevice device)
+    {
+        inputDevice = device;
+    }
+
+    public bool isRoleAssigned {
+        get {
+            return roleId != -1;
+        }
+    }
+}
