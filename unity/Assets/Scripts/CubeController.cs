89eb59568a4102d259d22596bf5d866ac8de05c9
Kasper Lind Sørensen <omglolkkthxbye@gmail.com>
2016-04-09 15:47:15 +0200

    CubeController now supports multiple controllers at once.

--- unity/Assets/Scripts/CubeController.cs
+++ unity/Assets/Scripts/CubeController.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using InControl;
 
 
 public class CubeController : MonoBehaviour {
+
+    [SerializeField]
+    private Transform[] cubes;
+    private List<Controller> controllers = new List<Controller>();
 
     void Start()
     {
@@ -17,9 +22,47 @@ public class CubeController : MonoBehaviour {
 
         // Update is called once per frame
         void Update () {
+        
 
-        var InputDevice0 = InputManager.ActiveDevice;
-        transform.Rotate(Vector3.down, 500.0f * Time.deltaTime * InputDevice0.LeftStickX, Space.World);
-        transform.Rotate(Vector3.right, 500.0f * Time.deltaTime * InputDevice0.LeftStickY, Space.World);
+        foreach(InputDevice device in InputManager.Devices){
+            if (FindControllerByInputDevice(device) == null) {
+                // New device spotted
+                controllers.Add(new Controller(device));
+            }
+            Controller activeController = FindControllerByInputDevice(device);
+            if (device.LeftStick.X < 0f){
+                // Pointing left
+                if (device.LeftStick.Y < 0f){
+                    // Pointing up
+                    activeController.roleId = 0;
+                } else if (device.LeftStick.Y > 0f){
+                    // Pointing down
+                    activeController.roleId = 1;
+                }
+            } else if (device.LeftStick.X > 0f) {
+                // Pointing right
+                if (device.LeftStick.Y < 0f){
+                    // Pointing up
+                    activeController.roleId = 2;
+                } else if (device.LeftStick.Y > 0f){
+                    // Pointing down
+                    activeController.roleId = 3;
+                }
+            }
+            
+            if (device.Action1.IsPressed) {
+//                Debug.Log("Action1");
+                if (activeController.isRoleAssigned) {
+                    Debug.Log(activeController.roleId);
+                    if (activeController.roleId < cubes.Length) {
+                        cubes[activeController.roleId].transform.Rotate(Vector3.up, 50f);
+                    }
+                }
+            }
+        }
+    }
+
+    private Controller FindControllerByInputDevice (InputDevice inputDevice) {
+        return controllers.Find((Controller controller) => {return controller.inputDevice == inputDevice;});
     }
 }
