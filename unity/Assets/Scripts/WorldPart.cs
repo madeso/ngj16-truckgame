f954ba344aadeaeddd024030b798fce249da4a7f
Gustav <sir.gustav.the.coder@gmail.com>
2016-04-10 12:12:41 +0200

    rolling stones!

--- unity/Assets/Scripts/WorldPart.cs
+++ unity/Assets/Scripts/WorldPart.cs
@@ -5,19 +5,27 @@ public class WorldPart : MonoBehaviour {
 
 	public List<GameObject> SpawnLocations;
 	public GameObject Cactus;
+	public GameObject Stone;
 	public int Count = 2;
 
 	void Start () {
 		if( this.SpawnLocations == null ) return;
 		if( this.SpawnLocations.Count == 0 ) return;
 
-		for(int i=0; i< this.Count; ++i) {
-			var id = Random.Range(0, this.SpawnLocations.Count);
-			var c = GameObject.Instantiate(this.Cactus);
-			c.transform.position = this.SpawnLocations[id].transform.position;
-			c.transform.rotation = Quaternion.AngleAxis(Random.Range(0, 360), Vector3.up);
+		if( this.Cactus != null ) SpawnObjects (this.Count, this.Cactus, 0);
+		if( this.Stone != null ) SpawnObjects (1, this.Stone, 8);
+	}
 
-			this.SpawnLocations.RemoveAt(id);
+	void SpawnObjects (int count, GameObject go, float x)
+	{
+		for (int i = 0; i < count; ++i) {
+			var id = Random.Range (0, this.SpawnLocations.Count);
+			var c = GameObject.Instantiate (go);
+			var p = this.SpawnLocations [id].transform.position;
+			p.y += x;
+			c.transform.position = p;
+			c.transform.rotation = Quaternion.AngleAxis (Random.Range (0, 360), Vector3.up);
+			this.SpawnLocations.RemoveAt (id);
 		}
 	}
 	
